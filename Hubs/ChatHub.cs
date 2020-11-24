ccef83cbc10a5536aa03db059fb9a2f63a79b297
laura.meikle <laura.meikle@apdcomms.co.uk>
2020-11-24 13:45:26 +0000

    initial commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+/**
+ * A hub is a class that serves as a high-level pipeline that handles client-server communication.
+ * The ChatHub class inherits from the SignalR Hub class. The Hub class manages connections, groups, and messaging.
+ */
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        /**
+         * The SendMessage method can be called by a connected client to send a message to all clients. 
+         * JavaScript client code that calls the method is shown later in the tutorial. 
+         * SignalR code is asynchronous to provide maximum scalability.
+         */
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Getting started with SignalR
+
+This repo was built following this simple tutorial:<br>
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr?view=aspnetcore-5.0&tabs=visual-studio
+
+Some more good reading:
+- https://docs.microsoft.com/en-us/aspnet/signalr/
+- https://www.codemag.com/article/1807061/Build-Real-time-Applications-with-ASP.NET-Core-SignalR
